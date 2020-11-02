a2342c48093c3838c9ea641689cb056941671732
Pavlo Pokhylenko <Pavlo_Pokhylenko@epam.com>
2020-11-03 01:22:36 +0200

    #Task3 Enhanced getProductsById endpoint. Added fail fast approach.

--- product-service-ts/src/handlers/getProductsById.ts
+++ product-service-ts/src/handlers/getProductsById.ts
@@ -1,5 +1,6 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import products from '../data/products-mock.json';
+import { getResponseObject } from '../utils/api.util';
 
 export const getProductsById: APIGatewayProxyHandler = async (event) => {
   console.log("getProductsById FN. Event: ", event);
@@ -7,31 +8,34 @@ export const getProductsById: APIGatewayProxyHandler = async (event) => {
 
   try {
     const { id } = event?.pathParameters || {};
-    const product = products.find(product => {
-      return product.title === id;
-    });
-
-    if (!product) {
+    if(!id) {
       const error = JSON.stringify(
-        {
-          statusCode: 404,
-          body: JSON.stringify({message: `No product with such id ${id}`}),
-        }
+        getResponseObject(400, {message: `No product id was provided`})
       );
 
       throw new Error(error);
     }
 
-    return {
-      statusCode: 200,
-      headers: {
+    const product = products.find(product => {
+      return product.title === id;
+    });
+    if (!product) {
+      const error = JSON.stringify(
+        getResponseObject(404, {message: `No product with such id ${id}`})
+      );
+
+      throw new Error(error);
+    }
+    
+    return getResponseObject(
+      200,
+      product,
+      {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Credentials': true,
-      },
-      body: JSON.stringify(product),
-    };
+      }
+    );
   } catch(error) {
     return JSON.parse(error.message);
-  }
-  
+  }  
 };
--- product-service-ts/src/handlers/getProductsList.ts
+++ product-service-ts/src/handlers/getProductsList.ts
@@ -1,16 +1,17 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import products from '../data/products-mock.json';
+import { getResponseObject } from '../utils/api.util';
 
 export const getProductsList: APIGatewayProxyHandler = async (event) => {
   console.log("getProductsList FN. Event: ", event);
-  return {
-    statusCode: 200,
-    headers: {
+  return getResponseObject(
+    200,
+    products,
+    {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Credentials': true,
-    },
-    body: JSON.stringify(products),
-  };
+    }
+  );
 };
 
 export default getProductsList;
